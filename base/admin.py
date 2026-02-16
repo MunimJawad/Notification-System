f44d4f0af5c57e2716e429924ce423fbdef0dcb6
MunimJawad <mnm.jwd01@gmail.com>
2026-02-16 15:44:35 +0600

    feat: Ticket, Notifications Models Added

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Ticket, Notifications
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Notifications)
--- base/models.py
+++ base/models.py
@@ -23,4 +23,49 @@ class User(AbstractUser, SoftDeleteModel):
     def __str__(self):
         return f"Username:{self.username} --Email: {self.email}"
     
+
+class Ticket(SoftDeleteModel):
+    PENDING = "pending"
+    SOLVED = "solved"
+    FAILED = "failed"
+
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (SOLVED, 'Solved'),
+        (FAILED, 'Failed')
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES, default=PENDING, db_index=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,  related_name="created_tickets")
+    assigned_to = models.ForeignKey(User, null= True, blank= True, on_delete=models.SET_NULL,   related_name="assigned_tickets")
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+       ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.title}----({self.created_at})"
+    
+
+class Notifications(SoftDeleteModel):
+    NOTIFICATION_TYPES = [
+        ('ticket_created', 'Ticket Created'),
+        ('ticket_status_updated', 'Ticket Status Updated'),
+    ]
+    users = models.ManyToManyField(User, related_name="notifications")
+    notification_type = models.CharField(max_length=40, choices=NOTIFICATION_TYPES)
+    title = models.CharField(max_length=200)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name="notifications")
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+
+
     
